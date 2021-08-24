8f0abb47e5293943abea59e33046f7824c21e7f8
suriya1410 <79642202+suriya1410@users.noreply.github.com>
2021-08-24 20:21:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
-import mysql.connector as mysql
 import logging
 app = Flask(__name__)
-logging.basicConfig(filename='application.log', level=logging.INFO,
-    format='%(levelname)s:%(message)s')
+logging.basicConfig(filename='flask.log', level=logging.INFO,format='%(levelname)s:%(message)s')
 
-app.config['MYSQL_HOST'] = 'database-my.caomyyms75ok.us-east-1.rds.amazonaws.com' 
+app.config['MYSQL_HOST'] = 'database-my.caomyyms75ok.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_USER'] = 'suriya'
 app.config['MYSQL_PASSWORD'] = 'suriya123'
 app.config['MYSQL_DB'] = 'regform'
 
-db = mysql.connect(
-    host = "database-my.caomyyms75ok.us-east-1.rds.amazonaws.com",
-    user = "suriya",
-    passwd = "suriya123"
-)
-cursor = db.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS reg")
-cursor.execute("USE reg")
-cursor.execute("CREATE TABLE IF NOT EXISTS userdata(name VARCHAR(150), age INT(3), email VARCHAR(150), mobile VARCHAR(10), location VARCHAR(100))") 
+
 
 mysql = MySQL(app)
 
@@ -35,20 +25,21 @@ def index():
         mobile = details['mobile']
         location = details['location']
         cur = mysql.connection.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS userdata(name VARCHAR(150), age INT(3), email VARCHAR(150), mobile VARCHAR(10), location VARCHAR(100))") 
         cur.execute("INSERT INTO userdata(name, age, email, mobile, location) VALUES (%s, %s, %s, %s, %s)", (name, age, email, mobile, location))
         mysql.connection.commit()
         cur.close()
         return 'success'
     return render_template('index.html')
 
+
 @app.route('/users')
 def users():
     cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM information")
-    if resultValue >0:
-        userDetails = cur.fetchall()
-        return render_template('users.html',userDetails=userDetails)
-
+    resultValue = cur.execute("SELECT * FROM userdata")
+    if resultValue > 0:
+        usersDetails = cur.fetchall()
+        return render_template('users.html',usersDetails=usersDetails)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=80)
+    app.run(host="0.0.0.0",port=80,debug=True)
